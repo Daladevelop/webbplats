61167fd776c903a95d97b1fcf2f11d153e23a2dc
Erlaan <firensonic@sandia-gaming.se>
2015-12-02 13:19:53 +0100

    Försök nr 2 att fixa issue #4!

--- daladevelop/comments.php
+++ daladevelop/comments.php
@@ -47,5 +47,37 @@
 		<p class="no-comments">Inga nya kommentarer kan lämnas.</p>
 	<?php endif; ?>
 
-	<?php comment_form(); ?>
+	<?php
+		$commenter = wp_get_current_commenter();
+		$req = get_option( 'require_name_email');
+		$aria_req = ($req ? " aria-required ='true'" :'');
+		
+		$comments_args = array(
+			//Redifine textarea
+			'comment_field' =>
+			         '<p class="comment-form-comment"><label for="comment">' . _x( 'Comment', 'noun') . '</label><br />
+				 <textarea id="comment" name="comment" aria-requird="true" placeholder="Skriv in din kommentar!" rows="8" cols="45" wrap="hard"></textarea></p>' ,
+				'fields' => apply_fillters( 'comment_form_default_fields', array(
+			
+			'author' =>
+			        '<p class="commet-form-author">' .
+				'<input id="author" placeholder="Ditt för och efternamn" name="author" type="text" value=""' . esc_attr( $commenter['comment_author']] .
+				'" size="30"' . $aria_req .' /> ' . ($req ? '<span style="color:red" class="required">*</span>' : ''). '</p>',
+				
+    		'email' =>
+				'<p class = comment-form-email">' .
+				'<input id ="email" placeholder="Din epostaddress!" name="email" type="text" value=""' .esc_attr($commenter['comment_author_email'] ) .
+				'" size="30"' . $aria_req . '/>' . ($req ? '<span style="color:red" class=required">*</span>' : '') . '</p>' ,
+
+		 	'url' =>
+				'<p class = comment-form-url">' .
+				'<input id ="url" placeholder="Hemsida" name="url" type="text" value="" ' .esc_attr($commenter['comment_author_url'] ) .
+				'" size="30"/>' . '</p>' 
+			)
+		),
+	);	      
+			  
+	comment_form($comments_args); ?>
+
+
 </div>
